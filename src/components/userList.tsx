1a64d2a02d0684bb60d733ca2394c2d47a7f8ee2
sunxingba <sunxingba@e-u.cn>
2019-07-26 18:27:37 +0800

    抽取Iuser 及调整页面显示数据列表

--- src/components/userList.tsx
+++ src/components/userList.tsx
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
+import { IUser } from './../model/user.model';
+import './userList.css'
 
-interface Props {
-
-}
+interface Props {}
 
 interface State {
-    users:[]
+    users:Array<IUser>
 }
 
 class User extends Component<Props,State>{
@@ -19,19 +19,26 @@ class User extends Component<Props,State>{
     componentWillMount(){
         fetch("http://localhost:8081/rest2/users")
             .then(res => res.json())
-            .then(data => {this.setState({users:data});
+            .then(data => {
+                this.setState({users:data});
                 console.log(data);
             })
     }
-    // [User{userId='437e4dcc-8b59-4f94-978d-e997fdef4c1e', userName='xiaoxi1', userSex='男', userAge=24, userNo='2019001', userPhoneNum='15592487001', createTime=Fri Jul 26 00:00:00 CST 2019, modifyTime=Fri Jul 26 00:00:00 CST 2019, userState='离线'}, User{userId='1b9be272-bbc2-46d1-88aa-244946d9c902', userName='xiaoxi2', userSex='女', userAge=24, userNo='2019002', userPhoneNum='15592487002', createTime=Fri Jul 26 00:00:00 CST 2019, modifyTime=Fri Jul 26 00:00:00 CST 2019, userState='隐身'}, User{userId='2cf37ec9-a98b-4d04-a2de-35dfd1b41132', userName='xiaoxi3', userSex='男', userAge=24, userNo='2019003', userPhoneNum='15592487003', createTime=Fri Jul 26 00:00:00 CST 2019, modifyTime=Fri Jul 26 00:00:00 CST 2019, userState='请勿打扰'}, User{userId='4dc38468-50be-4536-be4a-ed0ef354ac15', userName='xiaoxi4', userSex='女', userAge=24, userNo='2019004', userPhoneNum='15592487004', createTime=Fri Jul 26 00:00:00 CST 2019, modifyTime=Fri Jul 26 00:00:00 CST 2019, userState='忙碌'}, User{userId='894f077d-d358-4344-90ba-a9a402755241', userName='xiaoxi5', userSex='男', userAge=24, userNo='2019005', userPhoneNum='15592487005', createTime=Fri Jul 26 00:00:00 CST 2019, modifyTime=Fri Jul 26 00:00:00 CST 2019, userState='离开'}, User{userId='37bd08f5-de2e-49f6-af9d-45e40cba5ff0', userName='xiaoxi6', userSex='女', userAge=24, userNo='2019006', userPhoneNum='15592487006', createTime=Fri Jul 26 00:00:00 CST 2019, modifyTime=Fri Jul 26 00:00:00 CST 2019, userState='在线'}]
     render(){
-        const userList = this.state.users.map((item, index) => {
-            <div key='dd'>
-                {item}
-            </div>
-        });
         return(
-           <div>{userList}</div>
+           <div>
+               <table className="table">
+                   <thead>
+                    <th>姓名</th>
+                    <th>性别</th>
+                   </thead>
+                   <tbody>
+                        {this.state.users.map( user => {
+                            return <tr key={user.userId}> <td>{user.userName}</td> <td>{user.userSex}</td></tr>
+                        })}
+                    </tbody>
+                </table>
+           </div>
             );
     }
 }
---
+++ src/model/user.model.tsx
@@ -0,0 +1,11 @@
+export interface IUser {
+    userId?:string,
+    userName?:string,
+    userSex?:string,
+    userAge?:number,
+    userNo?:string,
+    userPhoneNum?:string,
+    createTime?:Date,
+    modifyTime?:Date,
+    userState?:string
+}

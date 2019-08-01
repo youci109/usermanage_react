320ba0d5b5393f6073cc87585f00670a73d45aae
sunxingba <sunxingba@e-u.cn>
2019-08-01 16:05:58 +0800

    action数据处理调用api 更换为使用 axios 方式

--- src/actions/index.tsx
+++ src/actions/index.tsx
@@ -1,19 +1,28 @@
-import * as constants from '../config/contant'
+import * as constants from '../config/contant';
 import { Dispatch } from 'react';
+import axios from 'axios';
 
 // 获取用户
 export const  userList = () => {
     console.log("获取用户action");
 
     return function (dispatch:Dispatch<any>) {
-            fetch("http://localhost:8081/rest2/users")
-        .then(res => res.json())
-        .then(posts =>
-             dispatch({
-            type: constants.FETCH_USERLIST,
-            payload: posts
-        })
-        )
+        axios.get("http://localhost:8081/rest2/users")
+            .then(res =>{
+                    dispatch({
+                        type: constants.FETCH_USERLIST,
+                        payload: res.data
+                        })
+                })
+        
+        // fetch("http://localhost:8081/rest2/users")
+        // .then(res => res.json())
+        // .then(posts =>
+        //         dispatch({
+        //         type: constants.FETCH_USERLIST,
+        //         payload: posts
+        //         })
+        //     )
     }
 }   
 
@@ -21,18 +30,29 @@ export const  userList = () => {
 export const addUser = (userBody:Object) => {
     console.log("添加用户action");
     return function (dispatch:Dispatch<any>) {
-        fetch("http://localhost:8081/rest/user", {
-            method: "post",
-            headers: {
-                "content-Type": "application/json"
-            },
-            body: JSON.stringify(userBody)
-        }).then(res => res.json()).then(user =>
-            dispatch({
-                type: constants.ADD_USER,
-                payload: user
-            })
-            )
+        axios.post("http://localhost:8081/rest/user",
+                    JSON.stringify(userBody),
+                    {method:"post",headers:{"content-Type": "application/json"}}
+                ).then(res=>{
+                    dispatch({
+                        type: constants.ADD_USER,
+                        payload: res.data
+                        })
+                })
+               
+        // fetch("http://localhost:8081/rest/user", {
+        //         method: "post",
+        //         headers: {
+        //             "content-Type": "application/json"
+        //         },
+        //         body: JSON.stringify(userBody)})
+        //     .then(res => res.json())
+        //     .then(user =>
+        //         dispatch({
+        //             type: constants.ADD_USER,
+        //             payload: user
+        //         })
+        //     )
     }
 }
 
@@ -41,17 +61,25 @@ export function deleteUser(userId:string|undefined, index:any) {
     console.log("删除用户action");
     
     return function (dispatch:Dispatch<any>) {
-        fetch("http://localhost:8081/rest/user?userId=" + userId , {
-                method: "delete",
-                headers: {
-                    "content-Type": "application/json"
-                }
-            }).then( delet => {
+        axios.delete("http://localhost:8081/rest/user?userId=" + userId)
+            .then(() => {
                 dispatch({
                     type: constants.DELETE_USER,
                     payload: index
                 })
-            })
+            }
+        )
+        // fetch("http://localhost:8081/rest/user?userId=" + userId , {
+        //         method: "delete",
+        //         headers: {
+        //             "content-Type": "application/json"
+        //         }
+        //     }).then( delet => {
+        //         dispatch({
+        //             type: constants.DELETE_USER,
+        //             payload: index
+        //         })
+        //     })
         
     }
 }
